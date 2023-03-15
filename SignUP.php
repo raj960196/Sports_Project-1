118e266a4ed1b253844a59f23b442723d4f23f9a
raj960196 <127954393+raj960196@users.noreply.github.com>
2023-03-15 22:28:27 +0530

    Update and rename SignUP.html to SignUP.php

--- SignUP.php
+++ SignUP.php
@@ -1,14 +1,18 @@
 <html>
-
+    <?php
+    $conn = mysqli_connect("localhost","root","","singup");
+    ?>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    
     <title>SignUP</title>
     <link rel="Stylesheet" href="SignUP.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
 </head>
 
 <body>
+<form action="#" method="POST">
     <div class="box">
         <div class="left">
             <div class="overlay">
@@ -18,14 +22,15 @@
         </div>
         <div class="right">
             <h5>SIGNUP</h5>
+            
             <div class="inputs">
-                <input type="text" placeholder="First Name">
+                <input type="text" name="first_name" placeholder="First Name">
                 <br>
-                <input type="text" placeholder="Last Name">
+                <input type="text"  name="last_name"placeholder="Last Name">
                 <br>
-                <input type="email" placeholder="E-mail">
+                <input type="email" name="email" placeholder="E-mail">
                 <br>
-                <input type="password" placeholder="Password">
+                <input type="password" name="password" placeholder="Password">
                 <br>
                 <input type="password" placeholder="Confirm Password">
                 <br>
@@ -40,11 +45,31 @@
             </div>
             <br>
             <div class="button">
-                <button>Submit</button>
+                <input type = "submit" name = "button" value = "signup"/>
                 <a href="index.html"><button>Home</button></a>
             </div>
         </div>
     </div>
+</form>
 </body>
 
+    <?php
+    if(isset($_POST["button"])){
+        $first_name=$_POST["first_name"];
+        $last_name=$_POST["last_name"];
+        $email=$_POST["email"];
+        $password=$_POST["password"];
+ 
+        $que="insert into sports_information values('','$first_name','$last_name','$email','$password')";
+        $result=mysqli_query($conn,$que);
+        if($result){
+            echo "data inserted successfully";
+           
+        } else{
+            echo "please try again...";
+        }
+    }
+   
+
+    ?>
 </html>
